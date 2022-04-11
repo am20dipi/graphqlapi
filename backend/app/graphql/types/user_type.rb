f4d2be5d2711255d91fabe316ca297e0117d2b94
Adriana <am20dipi@alum.siena.edu>
2022-04-11 16:23:01 -0400

    Creating entry points to querying all users or a specifc user in QueryType class; constructing box_count methon in UserType class to get the box count for each user

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -5,7 +5,14 @@ module Types
     field :id, ID, null: false
     field :email, String
     field :name, String
+    field :boxes, [Types::BoxType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :box_count, Integer, null: true
+
+    def box_count
+      object.boxes.count
+    end
   end
 end
